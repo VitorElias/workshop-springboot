0b33e99ff27d58de594b60e0a2f73a623bb58ae4
unknown <vitorepbf2004@gmail.com>
2025-08-21 23:49:14 -0300

    Criando projeto

---
+++ src/test/java/com/devVItor/WebServices/WebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devVItor.WebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
